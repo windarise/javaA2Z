29e29a5cb16c8ad6e23ff7a659d9dddbb3ac59b5
windarise <likewiseman@gmail.com>
2016-08-26 00:23:17 +0300

    add new files to projects javaA2Z

---
+++ src/main/java/varasimovich/Calculate.java
@@ -0,0 +1,8 @@
+package varasimovich;
+
+public class Calculate {
+	public static void main (String [] args) {
+		System.out.println("------------");
+		System.out.println("Hello, wolf ! said a Red Riding Hood..and discharge a gun shots 8-)");
+	}
+}	
